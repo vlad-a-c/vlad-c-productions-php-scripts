62d218f5dacc5a8ae838aea2ff4d8c9dcf5c8d83
vlad-a-c <vlad200003@gmail.com>
2019-06-10 01:46:45 +0300

    changes to the tt web creator
    added the config.php. now it's way more easier to change server addresses and stuff like that
    added the optional ability to store the data in a database. see the config.php for more details.

---
+++ tt web creator/config.php
@@ -0,0 +1,16 @@
+<?php
+$host="your TeamTalk server address";
+$port="10333";
+//note: you need to set the script to log in to one of the server's admin accounts in order to properly work
+$tt_user="your admin username";
+$tt_password="your admin password";
+$tt_nick="tt creator";
+//below is the database details for storring data.
+//first, we set a boolean variable so we see if we actually want to have the data stored into the database.
+$db_store=false;
+$db_host="localhost";
+$db_user="";
+$db_password="";
+$db_name="";
+$table_name="";
+?>
--- tt web creator/register.php
+++ tt web creator/register.php
@@ -1,22 +1,55 @@
 <?php
 include "tt5_api_lib.php";
-$host="your TeamTalk server address";
-$port="10333";
-//note: you need to set the script to log in to one of the server's admin accounts in order to properly work
-$user="your admin username";
-$password="your admin password";
+include "config.php";
+$username=$_POST['username'];
+$password=$_POST['passwd'];
+if($db_store)
+{
+$db_con=mysqli_connect($db_host, $db_user, $db_password, $db_name);
+if(!$db_con)
+{
+die(mysqli_connect_error());
+}
+$sql="show tables like '$table_name'";
+$res=mysqli_query($db_con, $sql);
+if(!$res)
+{
+die("the command for table checking can't be executed. ".mysqli_error($db_con));
+}
+if(mysqli_num_rows($res)==0)
+{
+$sql="create table ".$table_name." (id int unsigned auto_increment primary key, username varchar(50), password varchar(50), ip varchar(50))";
+if(!mysqli_query($db_con, $sql))
+{
+die("error creating the table. ".mysqli_error($db_con));
+}
+}
+$sql="select * from ".$table_name." where username='$username'";
+$res=mysqli_query($db_con, $sql);
+if(!$res)
+{
+die("error checking for the existence of a user in this table ".mysqli_error($db_con));
+}
+if(mysqli_num_rows($res)>0)
+{
+die("user exists");
+}
+$ip=$_SERVER['REMOTE_ADDR'];
+$sql="insert into ".$table_name." (username, password, ip) values ('$username', '$password', '$ip')";
+if(!mysqli_query($db_con, $sql))
+{
+die("everything went fine but the data adding process. the account can't be created. contact the administrator for solving this issue. show him the following error. <br/>".mysqli_error($db_con));
+}
+}
 $socket = fsockopen($host, $port, $errno, $errstr, 5.0);
 if(!$socket) die("there are some issues going on with the teamtalk server. please try again later");
 stream_set_timeout($socket, 5);
 $cmdid=1;
-fwrite($socket, "login username=\"$user\" password=\"$password\" protocol=5.0 nickname=\"tt creator\" id=$cmdid\r\n");
-$username=$_POST['username'];
-$password=$_POST['passwd'];
+fwrite($socket, "login username=\"$tt_user\" password=\"$tt_password\" protocol=5.0 nickname=\"$tt_nick\" id=$cmdid\r\n");
 if($username=="" or $password=="") die("you need to enter a username or password");
 $cmdid++;
 $userrights= UserRight::USERRIGHT_MULTI_LOGIN;
 $userrights|= UserRight::USERRIGHT_TRANSMIT_VOICE;
-//fwrite($socket, "newaccount username=\"$username\" password=\"$password\" usertype=2 note=\"created using the web script\" id=$cmdid\r\n");
 fwrite($socket, "newaccount username=\"$username\" password=\"$password\" usertype=1 userrights=$userrights note=\"created using the web script\" id=$cmdid\r\n");
 if(!process_reply_cmd($cmdid))
 {
